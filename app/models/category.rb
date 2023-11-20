bcee4f80a99154e176ea067e802e5c15f9916ea6
haperezv <arcaico13@hotmail.com>
2023-11-20 17:34:18 -0500

    se realizan cambios en el objeto de categoria

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,17 +1,3 @@
-# == Schema Information
-#
-# Table name: categories
-#
-#  id         :bigint           not null, primary key
-#  name       :string
-#  slug       :string
-#  created_at :datetime         not null
-#  updated_at :datetime         not null
-#
-# Indexes
-#
-#  index_categories_on_slug  (slug) UNIQUE
-#
 class Category < ApplicationRecord
     has_many :product_categories
     has_many :products
